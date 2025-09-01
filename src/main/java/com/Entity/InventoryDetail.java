a6eb8992c638b53cbdfd3d27578563baf250284d
Rahul Gadhave <rahulgadhave022@gmail.com>
2025-09-01 19:22:26 +0530

    some

--- src/main/java/com/Entity/InventoryDetail.java
+++ src/main/java/com/Entity/InventoryDetail.java
@@ -1,12 +1,13 @@
 package com.Entity;
 
 import jakarta.persistence.Entity;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-
+//entity class
 @Entity
 @Table(name = "inventory_Detail")
 public class InventoryDetail {
